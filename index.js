95b1e685c093b39c8b1a4976079afec0e22be4dd
Quangtien <Quangtientb97@gmail.com>
2020-06-15 21:45:29 +0700

    code moi

--- index.js
+++ index.js
@@ -7,18 +7,28 @@ var app = express();
 var server = require("http").createServer(app);
 var io = require("socket.io").listen(server);
 var fs = require("fs");
+var moment = require('moment-timezone');
 server.listen(8080);
 require('events').EventEmitter.prototype._maxListeners = 100;
-/*var con = mysql.createConnection({
+var con = mysql.createConnection({
  host: "b034kdbmfuvinopgjuse-mysql.services.clever-cloud.com",
   user: "u20nnlbcqemoj3jy",
   password: "t7zRtkGhq0F1svEcGKlC",
    database: "b034kdbmfuvinopgjuse"
-});*/
+});
+
+
 var ketqua;
-var con;
+var device = {};
+var app = {};
+var app_control = {};
+
 
 handleDisconnect();
+
+
+//var now = moment();
+
 
 
 /*password ---------------------------------------------------------*/
@@ -54,6 +64,7 @@ function checkHashPassword(userPassword,salt){
 
 /*bat su kien ket noi server-------------------------------------------------*/
 io.sockets.on('connection', function(socket){
+	var time = moment().tz('Asia/Ho_Chi_Minh').format('YYYY-MM-DD HH:mm:ss');
 	// dang ki tai khoan
 	socket.on('client-dang-ki-user', function(data){
 		//var ketqua;
@@ -71,7 +82,7 @@ io.sockets.on('connection', function(socket){
 	  
 		con.query('SELECT * FROM users where email=?',[email], function(err,result, fields){
 			con.on('error',function(err){
-				console.log('mysql error 78',err);
+				console.log('mysql error 78',err.code);
 			});
 
 			if (result && result.length){
@@ -81,20 +92,11 @@ io.sockets.on('connection', function(socket){
 			}
 			else{
 				ketqua = true;
-				let sql1 = `INSERT INTO users(unique_id, name, email, encrypted_password, salt, create_at) values (  \'${uid}\', \'${name}\', \'${email}\', \'${password}\', \'${salt}\', CURTIME())` ;
+				let sql1 = `INSERT INTO users(unique_id, name, email, encrypted_password, salt, create_at) values (  \'${uid}\', \'${name}\', \'${email}\', \'${password}\', \'${salt}\', \'${moment().tz('Asia/Ho_Chi_Minh').format('YYYY-MM-DD HH:mm:ss')}\')` ;
 				con.query(sql1, function (err) {
-						console.log('mysql error 90',err);
+						console.log('mysql error 90',err.code);
 						//console.log('khong thanh cong');						
 				});
-				console.log('thanh cong');
-				/*con.query('INSERT INTO `user`(`unique_id`, `name`, `email`, `encrypted_password`, `salt`, `create_at`, `updated_at`) VALUES (?,?,?,?,?,NOW(),NOW())',[uid,name,email,password,salt],function(err,result, fields){
-					con.on('error',function(err){
-						console.log('mysql error',err);
-						console.log('khong thanh cong');						
-				});
-				console.log('thanh cong');
-				//socket.emit('ket-qua-dang-ki',{noidung: ketqua});
-				});*/
 			}
 			socket.emit('ket-qua-dang-ki',{noidung: ketqua});
 		});	
@@ -102,11 +104,10 @@ io.sockets.on('connection', function(socket){
 	// dang nhap
 	socket.on('client-dang-nhap-user', function(data){
 		var email    	  = data.email;
-		var user_password = data.password;
-		
+		var user_password = data.password;		
 		con.query('SELECT * FROM users where email=?',[email], function(err,result, fields){
 			con.on('error',function(err){
-				console.log('mysql error 113',err);
+				console.log('mysql error 113',err.code);
 			});
 			if (result && result.length){
 				var salt = result[0].salt;
@@ -115,8 +116,10 @@ io.sockets.on('connection', function(socket){
 				if (encrypted_password == hashed_password) {
 					ketqua = true;
 					//res.end(JSON.stringify(result[0]));
+					// ô đẩy git cái phụ giống hệt cái hiện tại bên ô lên đc k
 					console.log('dang nhap thanh cong');
 					console.log(result[0]);
+					console.log('app socket.id: ' + socket.id);
 				}
 				else{
 					ketqua = false;
@@ -129,49 +132,79 @@ io.sockets.on('connection', function(socket){
 			}
 			socket.emit('ket-qua-dang-nhap',{noidung: ketqua});
 		});
+		con.query('SELECT unique_id FROM users where email=?',[email], function(err,result, fields){
+			con.on('error',function(err){
+				console.log('mysql error 78',err.code);
+			});
+			if (result && result.length){
+				app[result[0].unique_id] = socket.id;
+			}
+		});
 	});
 	//update_data device
 	socket.on('update_data', function(data){
-		console.log(data);
-		let sql = `CREATE TABLE IF NOT EXISTS device${data.device_id}_log (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,ThoiGian DATETIME, chieuquay VARCHAR(255), tocdo INT(10)) ENGINE = InnoDB` ;
+		//console.log(data);
+		//console.log('socket id la: ' + socket.id);
+		let sql = `CREATE TABLE IF NOT EXISTS device${data.device_id}_log (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,ThoiGian TIMESTAMP, chieuquay VARCHAR(255), tocdo INT(10)) ENGINE = InnoDB` ;
 		con.query(sql, function(err){
 			con.on('error', function(err){
-				console.log('mysql error 142',err);
+				console.log('mysql error 142',err.code);
 			});
 		});
-		sql = `INSERT INTO device${data.device_id}_log(chieuquay, tocdo) values (  \'${data.chieuquay}\', \'${data.tocdo}\')`;
+		sql = `INSERT INTO device${data.device_id}_log(chieuquay, tocdo, Thoigian) values (  \'${data.chieuquay}\', \'${data.tocdo}\', \'${moment().tz('Asia/Ho_Chi_Minh').format('YYYY-MM-DD HH:mm:ss')}\')`;
+		//console.log(sql);
 		con.query(sql, function(err){
 			con.on('error', function(err){
-				console.log('mysql error 148',err);
+				console.log('mysql error 148',err.code);
 			});
-		});		
+		});	
+		socket.to(app[app_control[data.device_id]]).emit('send-app', data);
+		//console.log('sent to' + app[app_control[data.device_id]]);
+
 	});
 	//join room
 	socket.on('join-room-device', function(data){
-		var device_id = data;
-		con.query('SELECT `unique_id` FROM devices where device_id=?',[device_id], function(err,result, fields){
+		device[data] = socket.id;
+		console.log('device id: ' + socket.id);
+		con.query('SELECT unique_id FROM devices where device_id=?',[data], function(err,result, fields){
 			con.on('error',function(err){
-				console.log('mysql error 157',err);
+				console.log('mysql error 78',err.code);
 			});
-			//console.log(result[0].unique_id);
-			var room = result[0].unique_id;
-			console.log(socket.id + "da join phong: " + result[0].unique_id);
-			socket.join(room);
-			console.log(socket.adapter.rooms);
+			app_control[data] = result[0].unique_id;
+		});
+
+	});
+	//dieu khien motor
+	socket.on('receive-motor', function(data){
+		console.log(data);
+		var device_id = data.device_id;
+		var json = `{"rota":${data.rota},"mode":${data.mode}}`;	
+		const obj = JSON.parse(json);
+		socket.to(device[device_id]).emit('send-motor', obj);
+		console.log('send to ' + device[device_id]);
+		con.query(`SELECT COUNT(*) AS so_luong FROM device${data.device_id}_log`, function(err,result, fields){
+			con.on('error',function(err){
+				console.log('mysql error 179',err.code);
+			});
+			if (result && result.length){
+				if (result[0].so_luong > 50000){
+					con.query(`DELETE FROM device${data.device_id}_log`, function(err,result, fields){
+						con.on('error',function(err){
+								console.log('mysql error 184',err.code);
+						});	
+					console.log(`DELETE FROM device${data.device_id}_log`);			
+					});
+				}
+			}
 		});	
 	});
-	/*socket.on('join-room-app', function(data){
-		var email = data.email;
-		con.query('SELECT * FROM users where email = ? ',[email], function(err,result, fields){
-			con.on('error',function(err){
-				console.log('mysql error 170',err);
-			});
-			var room = result[0].unique_id;
-			//console.log(result[0].unique_id);
-			socket.join(room);
-			console.log(socket.id + "da join phong: " + room);
-		});	
-	});*/
+	socket.on('disconnect', function(data){
+		console.log(socket.id + ' disconnect');
+	});	
+
+
+	
+
 
 
 
@@ -179,7 +212,7 @@ io.sockets.on('connection', function(socket){
 //end io
 });
 
-//
+
 function handleDisconnect() {
 	 con = mysql.createConnection({
 	 host: "b034kdbmfuvinopgjuse-mysql.services.clever-cloud.com",
@@ -190,21 +223,19 @@ function handleDisconnect() {
 
 	con.connect(function(err) {              // The server is either down
 	    if(err) {                                     // or restarting (takes a while sometimes).
-	      console.log('error when connecting to db:', err);
+	      console.log('error when connecting to db: ', err);
 	      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-	    }                                     // to avoid a hot loop, and to allow our node script to
+	    }  
+	    console.log('ket noi lai thanh cong');                                   // to avoid a hot loop, and to allow our node script to
 	});                                     // process asynchronous requests in the meantime.
 	                                          // If you're also serving http, display a 503 error.
 	con.on('error', function(err) {
-	    console.log('db error', err);
+	    console.log('mysql error', err.code);
 	    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
 	      handleDisconnect();
 	      console.log("ket noi lai");                         // lost due to either server restart, or a
 	    } else {                                      // connnection idle timeout (the wait_timeout
-	      throw err;                                  // server variable configures this)
+	      console.log('mysql error handle',err);                                  // server variable configures this)
 	    }
 	});
 }
-
-
-
